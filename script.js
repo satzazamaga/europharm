d37669bff3213d2691bce8257ee9d41f79de7ba8
satzazamaga <vb507019@gmail.com>
2025-03-29 21:03:28 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-function showTab(tabId) {
-    // Скрываем все вкладки
-    const tabs = document.querySelectorAll('.tab-content');
-    tabs.forEach(tab => {
-        tab.classList.remove('active');
-    });
-
-    // Показываем выбранную вкладку
-    const selectedTab = document.getElementById(tabId);
-    selectedTab.classList.add('active');
-}
+document.addEventListener("DOMContentLoaded", function() {
+  console.log("Сайт загружен и готов к использованию.");
+});
