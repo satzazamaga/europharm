154d76ad659ea8e7acf9dba61bf29b8224e2aa42
satzazamaga <vb507019@gmail.com>
2025-03-29 20:20:07 +0500

    Update script.js

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ function showTab(tabId) {
     // Скрываем все вкладки
     const tabs = document.querySelectorAll('.tab-content');
     tabs.forEach(tab => {
-        tab.style.display = 'none';
+        tab.classList.remove('active');
     });
 
     // Показываем выбранную вкладку
     const selectedTab = document.getElementById(tabId);
-    selectedTab.style.display = 'block';
+    selectedTab.classList.add('active');
 }
